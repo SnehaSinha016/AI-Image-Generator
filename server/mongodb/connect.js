9f3cc1d9950092d02ccdd60ec6ec510596154706
SnehaSinha016 <sneha.sinha1610@gmail.com>
2025-07-19 23:43:01 +0530

    Update connect.js

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
-const connectDB=(url)=>{
-    mongoose.set('strictQuery',true);
-    return mongoose.connect(url)
-    .then(()=>console.log('MongoDB Connected'))
-    .catch((err)=>{
-        console.log(err)
-        process.exit(1);
-});
-    
-}
+
+const connectDB = async (url) => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(" MongoDB connection error:", err.message);
+    process.exit(1); 
+  }
+};
+
 export default connectDB;
