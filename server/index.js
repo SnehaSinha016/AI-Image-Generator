cf112c4af34c2891193bd574657d64579d15f7d4
SnehaSinha016 <sneha.sinha1610@gmail.com>
2025-07-20 11:11:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import stability from './routes/stability.js'
   const PORT = process.env.PORT || 8000;
   app.use(cors(
     {
-      origin:'https://ai-image-generator-ee5y.vercel.app/',
+      origin:'https://ai-image-generator-fmfg-git-main-snehasinha016s-projects.vercel.app/',
       methods:['GET','POST']
     }
   ));
